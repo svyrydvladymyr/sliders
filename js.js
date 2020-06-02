29d1f735b3e818dd3ef984284df27538f5219fe2
Svyryd Vladymyr <svyrydvladimir@gmail.com>
2020-06-02 16:35:27 +0300

    change styles

--- js.js
+++ js.js
@@ -1,12 +1,11 @@
-const add_event = document.getElementsByClassName('kalc_sl_wrap');
+const add_event = document.getElementsByClassName('kalc_sl_wrap'), active = 'kalc_active', title = 'kalc_sl_title';
 for (let i = 0; i < add_event.length; i++){
-    const event = add_event[i].getElementsByClassName('kalc_sl_title');
+    const event = add_event[i].getElementsByClassName(title);
     for (let i = 0; i < event.length; i++) {
         event[i].addEventListener('click', function() {
-            let child_title = this.parentElement.getElementsByClassName('kalc_sl_title');
-            let active_sl = this.classList.contains("kalc_active");
-            for (const value of child_title) { value.classList.remove("kalc_active") };             
-            if (active_sl !== true){ this.classList.toggle("kalc_active") };
+            const active_sl = this.classList.contains(active);
+            for (const value of this.parentElement.getElementsByClassName(title)) { value.classList.remove(active) };             
+            !active_sl ? this.classList.toggle(active) : null;
         });        
     }
 } 
