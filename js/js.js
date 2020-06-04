16ef2af00a5a30aeeb9634bab84ebaec32b0ec2b
Володимир Свирид <vsvyryd@ifnmu.edu.ua>
2020-06-04 23:07:06 +0300

    finish work at slider

--- js/js.js
+++ js/js.js
@@ -1,52 +1,13 @@
-// let fun = (el) => {    
-//     const add_event = el.nextElementSibling.getElementsByClassName('kalcifer_slider'), active = 'active', title = 'title';
-//     for (let i = 0; i < add_event.length; i++){
-//         const event = add_event[i].getElementsByClassName(title);
-//         for (let i = 0; i < event.length; i++) {
-//             event[i].addEventListener('click', function() {
-//                 const active_sl = this.classList.contains(active);
-//                 for (const value of this.parentElement.getElementsByClassName(title)) { value.classList.remove(active) };             
-//                 !active_sl ? this.classList.toggle(active) : null;
-//             });        
-//         }
-//     }
-// } 
 
+const title = 'title', active = 'active', event = document.getElementsByClassName(title);
+for (let i = 0; i < event.length; i++) {
+    event[i].addEventListener('click', function() {
+        const active_sl = this.classList.contains(active);
+        for (const value of this.parentElement.getElementsByClassName(title)) { value.classList.remove(active) };             
+        !active_sl ? this.classList.toggle(active) : null;
 
-const add_event = document.getElementsByClassName('kalcifer_slider'), active = 'active', title = 'title';
-for (let i = 0; i < add_event.length; i++){
-    const event = add_event[i].getElementsByClassName(title);
-    for (let i = 0; i < event.length; i++) {
-
-        event[i].addEventListener('click', function() {
-            const add_event = this.nextElementSibling.getElementsByClassName('kalcifer_slider');
-            for (let i = 0; i < add_event.length; i++){
-                const event = add_event[i].getElementsByClassName(title);
-                for (let i = 0; i < event.length; i++) {
-                    event[i].addEventListener('click', function() {
-                        const active_sl = this.classList.contains(active);
-                        for (const value of this.parentElement.getElementsByClassName(title)) { value.classList.remove(active) };             
-                        !active_sl ? this.classList.toggle(active) : null;
-                    });        
-                }
-            }
-        });  
-        
-        event[i].addEventListener('click', function() {
-            const active_sl = this.classList.contains(active);
-            for (const value of this.parentElement.getElementsByClassName(title)) { value.classList.remove(active) };             
-            !active_sl ? this.classList.toggle(active) : null;
-        });      
-        
-  
-    }
-} 
-
-
-
-
-
-
+    });    
+}
 
 
 
