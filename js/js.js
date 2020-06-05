0a4599117ba05bfeadc99281c898e8cb16d31fda
Svyryd Vladymyr <svyrydvladimir@gmail.com>
2020-06-05 15:28:54 +0300

    fix bugs

--- js/js.js
+++ js/js.js
@@ -1,11 +1,9 @@
-
 const title = 'title', active = 'active', event = document.getElementsByClassName(title);
 for (let i = 0; i < event.length; i++) {
     event[i].addEventListener('click', function() {
         const active_sl = this.classList.contains(active);
         for (const value of this.parentElement.getElementsByClassName(title)) { value.classList.remove(active) };             
         !active_sl ? this.classList.toggle(active) : null;
-
     });    
 }
 
