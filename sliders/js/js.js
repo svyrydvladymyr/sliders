aab3a70777c88df26069462a71dfdf8bc050e541
Володимир Свирид <vsvyryd@ifnmu.edu.ua>
2020-06-11 22:57:50 +0300

    fix bugs

--- sliders/js/js.js
+++ sliders/js/js.js
@@ -1,8 +1,15 @@
-const title = 'title', active = 'active', event = document.getElementsByClassName(title);
-for (let i = 0; i < event.length; i++) {
-    event[i].addEventListener('click', function() {
+const title = 'title', active = 'active', events = document.getElementsByClassName(title);
+for (const event of events) {
+    event.addEventListener('click', function() {
         const active_sl = this.classList.contains(active);
         for (const value of this.parentElement.getElementsByClassName(title)) { value.classList.remove(active) };             
         !active_sl ? this.classList.toggle(active) : null;
-    });    
+    }); 
 }
+// for (let i = 0; i < event.length; i++) {
+//     event[i].addEventListener('click', function() {
+//         const active_sl = this.classList.contains(active);
+//         for (const value of this.parentElement.getElementsByClassName(title)) { value.classList.remove(active) };             
+//         !active_sl ? this.classList.toggle(active) : null;
+//     });    
+// }
